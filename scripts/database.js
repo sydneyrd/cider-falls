21ba212e955f04d36ebae3276e8a0f60ea282d4d
Sydney Dickson <sydneyrachelledickson@gmail.com>
2022-05-03 15:56:20 -0500

    some files

---
+++ scripts/database.js
@@ -0,0 +1,28 @@
+const database = [
+    const guests = [
+        {
+            id: 1,
+            name: "Connor Lopshire",
+        },
+        {
+            id: 2,
+            name: "Wes Harllee",
+        },
+        {
+            id: 3,
+            name: "Mary Stevens",
+        },
+        {
+            id: 4,
+            name: "Smokey the Bear",
+        },
+        {
+            id: 5,
+            name: "JFK",
+        },
+        {
+            id: 6,
+            name: "BTK"
+        }
+    ]
+]
