f40c45f20a21a0f6e5b7fa2ff34b1a405955e740
Wesley Harllee <wesharllee@gmail.com>
2022-05-04 09:48:26 -0500

    Database is Complete

--- scripts/database.js
+++ scripts/database.js
@@ -1,5 +1,5 @@
-const database = [
-    const guests = [
+const database = {
+    guests: [
         {
             id: 1,
             name: "Connor Lopshire",
@@ -24,5 +24,151 @@ const database = [
             id: 6,
             name: "BTK"
         }
+    ],
+
+    parks: [
+        {
+            id: 1,
+            name: "Chamfort River",
+        },
+        {
+            id: 2,
+            name: "Lost Wolf Hiking Trail",
+        },
+        {
+            id: 3,
+            name: "The Lodge",
+        },
+        {
+            id: 4,
+            name: "Gander River",
+        },
+        {
+            id: 5,
+            name: "Campgrounds",
+        },
+        {
+            id: 6,
+            name: "Pine Bluffs Trail"
+        }
+    ],
+
+    services: [
+        {
+            id: 1,
+            serviceType: "Rafting",
+        },
+        {
+            id: 2,
+            serviceType: "Canoeing",
+        },
+        {
+            id: 3,
+            serviceType: "Fishing",
+        },
+        {
+            id: 4,
+            serviceType: "Hiking",
+        },
+        {
+            id: 5,
+            serviceType: "Picnicking",
+        },
+        {
+            id: 6,
+            serviceType: "Rock Climbing",
+        },
+        {
+            id: 7,
+            serviceType: "Lodging",
+        },
+        {
+            id: 8,
+            serviceType: "Parking",
+        },
+        {
+            id: 9,
+            serviceType: "Information",
+        },
+        {
+            id: 10,
+            serviceType: "Zip-Lining",
+        },
+    ],
+    parkServices: [
+        {
+            id: 1,
+            serviceId: 1,
+            parkId: 1
+        },
+        {
+            id: 2,
+            serviceId: 2,
+            parkId: 1
+        },{
+            id: 3,
+            serviceId: 3,
+            parkId: 1
+        },{
+            id: 4,
+            serviceId: 4,
+            parkId: 2
+        },{
+            id: 5,
+            serviceId: 5,
+            parkId: 2
+        },{
+            id: 6,
+            serviceId: 6,
+            parkId: 2
+        },{
+            id: 7,
+            serviceId: 7,
+            parkId: 3
+        },{
+            id: 8,
+            serviceId: 8,
+            parkId: 3
+        },{
+            id: 9,
+            serviceId: 9,
+            parkId: 3
+        },{
+            id: 10,
+            serviceId: 5,
+            parkId: 3
+        },{
+            id: 11,
+            serviceId: 3,
+            parkId: 4
+        },{
+            id: 12,
+            serviceId: 4,
+            parkId: 4
+        },{
+            id: 13,
+            serviceId: 9,
+            parkId: 5
+        },{
+            id: 14,
+            serviceId: 7,
+            parkId: 5
+        },{
+            id: 15,
+            serviceId: 8,
+            parkId: 5
+        },{
+            id: 16,
+            serviceId: 4,
+            parkId: 6
+        },{
+            id: 17,
+            serviceId: 5,
+            parkId: 6
+        },{
+            id: 18,
+            serviceId: 10,
+            parkId: 6
+        }
     ]
-]
+}
