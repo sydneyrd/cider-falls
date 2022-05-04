212338af050be8860355927f319d30668cac156c
Connor Lopshire <lopshire.connor@gmail.com>
2022-05-04 13:30:54 -0500

    added initial park card function to display park services

--- scripts/parks.js
+++ scripts/parks.js
@@ -1,13 +1,34 @@
 import { getParks } from "./database.js";
 import { getParkServices } from "./database.js";
-
+import { getServices } from "./database.js";
 // write  function that iterates through parks and displays them by name 
 //  correlate services to park 
 
 export const parkList = () => {
-    const parks = getParks()
-    const parkServices = getParkServices()
-
-
+    
+    
 }
+const parkServices = getParkServices()
+const parks = getParks()
+
+
+
+
+const services = getServices ()
+ export const serviceHTML = (parkId) => {
+     let serviceHTMLString = ""
+     for ( const parkService of parkServices) {
+        if (parkId === parkService.parkId) {
+            let serviceId = parkService.serviceId
+            for (const service of services) {
+                if (service.id === parkService.serviceId) {
+                    serviceHTMLString += `<div>${service.serviceType}</div>`
+        }
+             
+        }
+    }
+}
+return serviceHTMLString 
+}
+
 
