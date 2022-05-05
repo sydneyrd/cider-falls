1fdf0a5fa2a8359450f0ae6a3e641908188ae731
Wesley Harllee <wesharllee@gmail.com>
2022-05-05 10:02:55 -0500

    checkout WH-camp. Things are on the screen now

---
+++ scripts/header.js
@@ -0,0 +1,9 @@
+export const header = () => {
+    let htmlString = ""
+    htmlString += 
+    `<header>
+        
+        <img  class="logo__image image--card" src="https://i.ibb.co/3CzYySV/Cider-Falls.png" />
+    </header>`
+    return htmlString
+}
