feaeeaae150f054766d6d6e88cf0785af5282423
Connor Lopshire <lopshire.connor@gmail.com>
2022-05-05 10:21:49 -0500

    added and deployed footer

---
+++ scripts/footer.js
@@ -0,0 +1,11 @@
+export const footer = () => {
+    let htmlString = ""
+    htmlString += 
+    `<footer>
+        <div>1800-get-cash-now</div>
+        <div>ciderfalls@hotmail.com</div>
+        <div>1111 Road rd S Somehwere, SomeTown 33333</div>
+
+    </footer>`
+    return htmlString
+}
