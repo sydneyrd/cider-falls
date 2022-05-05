59d30307c050df098a5694e22f8cd0f05ffb4b43
Sydney Dickson <sydneyrachelledickson@gmail.com>
2022-05-05 10:47:00 -0500

    css stuff

--- scripts/main.js
+++ scripts/main.js
@@ -7,11 +7,11 @@ import { header } from "./header.js";
 
 const applicationHTMLString = `${header()}
 <article class="main">
-    <section class="main--column list main__parkList">
+    <section class="main__parkList">
         <h2>Parks and Services</h2>
         ${parkListHTML()}
     </section>
-    <section class="main--column list mains__guestList">
+    <section class="main__guest">
         <h2>Guests</h2>
         ${guestHTML()}
     </section>
